ced630bd92a9560e38a69772638527eb23cb74c0
Liam Schauerman <ljschauerman@gmail.com>
2015-02-05 11:14:04 -0800

    styling

--- remote.js
+++ remote.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	var host = document.location.origin;
 	var socket = io.connect(host); 
 	socket.on('connect', function(data){
-		console.log('connected')
+		console.log('connected');
+
 		$('#cast').click(function(){
 			socket.emit('shell');
 		})
+		
 	});
 })
