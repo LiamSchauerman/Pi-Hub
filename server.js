89102d8d0448c99b94e5200a27e9d7812c702e92
Liam Schauerman <ljschauerman@gmail.com>
2015-02-04 20:57:42 -0800

    run_shell implemented

--- server.js
+++ server.js
@@ -10,8 +10,18 @@ var io = require("socket.io").listen(server);
 app.use(express.static(__dirname + '/'))
 
 server.listen(port, function(){
-  console.log('Listening on port ' + port);
+	console.log('Listening on port ' + port);
 });
+
+// UTILITY
+function run_shell(cmd, args, cb, end) {
+    var child = spawn(cmd, args),
+        me = this;
+    child.stdout.on('data', function (buffer) { cb(me, buffer); });
+    child.stdout.on('end', end);
+}
+
+
 
 // ROUTES
 app.get('/', function (req, res) {
@@ -24,6 +34,16 @@ io.sockets.on('connection', function(socket){
 	socket.on('newUrl', function(data){
 		console.log("newUrl event heard");
 		console.log("we should redirect to "+data.url);
+		// var command = "chromium "+ data.url;
+		var runShell = new run_shell('chromium', [data.url], 
+			function (me, buffer) {
+			    me.stdout += buffer.toString();
+			    socket.emit("loading",{output: me.stdout});
+			    console.log(me.stdout);
+			 },
+			function () {
+				console.log("NewUrl event complete")
+			})
 	});
 })
 
