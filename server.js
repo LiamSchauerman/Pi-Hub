6b9558a4728795fbfd684a0c6b58e5f4549a1e8b
Liam Schauerman <ljschauerman@gmail.com>
2015-02-26 20:30:39 -0800

    forward

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ io.sockets.on('connection', function(socket){
 		child.stdin.write('q');
 	});
 	socket.on('forward', function(){
-		child.stdin.write('o');
+		child.stdin.write('^[[C');
 	});
 })
 
